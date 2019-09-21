db8fd0b7b390ef6881f61ed2ff4e4fb74529a9c9
nosangho <nosangho@gmail.com>
2019-09-21 21:45:25 +0900

    Post -> Listview

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+
+class Postlist(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all()
+# def index(request):
+#     posts = Post.objects.all()
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
