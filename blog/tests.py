fba69a052eb4994358f79a0448afff52a37de016
nosangho <nosangho@gmail.com>
2019-09-28 17:11:49 +0900

    make Post_list test page

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
 
 # Create your tests here.
+class TestView(TestCase):
+    def SetUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
