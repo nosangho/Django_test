36715ea3dff9f8a2ef0e539fce6a4dc3fa45ab14
nosangho <nosangho@gmail.com>
2019-09-30 21:07:54 +0900

    post detail page update

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,22 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from. models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+def create_post(title, content, author):
+    blog_post = Post.obects.create(
+        title = title,
+        content =  content,
+        created = timezone.now(),
+        author = author
+    )
+
+    return blog_post
+
+
 class TestView(TestCase):
     def SetUp(self):
         self.client = Client()
