0d53a1f2eb3ed7d530d105b701d3b8c07ecef17a
nosangho <nosangho@gmail.com>
2019-10-07 23:03:40 +0900

    category in post_list

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -9,6 +16,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
     
     def __str__(self):
         return '{}:{}'.format(self.title, self.author)
--- blog/tests.py
+++ blog/tests.py
@@ -1,20 +1,28 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from. models import Post
+from. models import Post, Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your tests here.
 
-def create_post(title, content, author):
+def create_post(title, content, author, category=None):
     blog_post = Post.obects.create(
         title = title,
         content =  content,
         created = timezone.now(),
-        author = author
+        author = author,
+        category = category,
     )
 
     return blog_post
+
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name = name,
+        description = description
+    )
+    return category
 
 
 class TestView(TestCase):
@@ -29,3 +37,21 @@ class TestView(TestCase):
 
         navbar = soup.find('div', id='navbar')
         self.assertIn('Blog', navbar.text)
+
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='nosangho', password='8121438n')
+
+    def test_category(self):
+        category = create_category()
+
+    # def test_post(self):
+    #     category = create_category()
+    #     post_000 = create_post(
+    #         title='first post',
+    #         content = 'hello world welcome to python',
+    #         author = self.author_000,
+    #         category = category
+    #     )
